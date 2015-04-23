26444d3c8b6401e8fd2c0be9f3edac2452898236
PeterBarsa <peter.barsa@gmail.com>
2015-04-23 23:59:32 +0200

    -unnessecary code comments + new function
    removed commentaries that was added to annoy the teacher, added a new
    private function in a repository as getting user code was replicated
    many times.

--- DatingSite_Projekt/DatingSite_Projekt/Controllers/AccountController.cs
+++ DatingSite_Projekt/DatingSite_Projekt/Controllers/AccountController.cs
@@ -14,35 +14,29 @@ using Dating_data.Repository;
 
 namespace DatingSite_Projekt.Controllers
 {
-    //säger att användaren måste vara igenkännbar för att komma åt denna klass
     [Authorize]
     public class AccountController : Controller
     {
         
-        //hämtar en vy för Profile
         public ActionResult Profile()
         {
 
-            //skapar en modell av typen AccountUserModel
-            var model = new AccountUserModel();
             //fyller i informationen i en model för den nuvarande inloggade användern med hjälp av namnet på kakan
+            var model = new AccountUserModel();
             model.Username = UserRepositories.GetUserName(IdentityHelper.CurrentUserId());
             var results = DescriptionRepository.GetDescription(IdentityHelper.CurrentUserId());
-            //konverterar en nullable int från resultatet till en vanlig int så den kan användas i modellen
             int age = results.Age ?? default(int);
-            //överför informationen hämtad ovan från variabeln results och för över till modellen
             model.Description = results.Description1;
             model.AboutMe = results.AboutMe;
             model.Age = age; 
             model.City = results.City;
             model.Country = results.Country;
             model.Email = results.Email;
-            //Hämtar meddelandena för användaren som är inloggad
+
             var messageList = new List<MessageModel>();
             var tempListMessage = MessageRepository.GetMessageList(IdentityHelper.CurrentUserId());
             foreach (var m in tempListMessage)
             {
-                //skapar en ny modell av alla Messages som hämtas och för in i listan för att sedan returneras tillsidan
                 var message = new MessageModel { Message = m.Messages, SenderName = UserRepositories.GetUserName(m.SenderId), SenderId = m.SenderId };
                 messageList.Add(message);
             }
@@ -51,31 +45,27 @@ namespace DatingSite_Projekt.Controllers
             ViewData["Message"] = messageList;
             ViewData["MessageModel"] = new MessageModel();
             ViewData["senderId"] = IdentityHelper.CurrentUserId();
-            //skickar med modellen in i vyn
             return View(model);
         }
         
-        //hämtar en vy för Friends
         public ActionResult Friends()
         {
-            //skapar en lista av typen FriendsModel
+
             var friendList = new List<FriendsModel>();
-            //hämtar alla vänner för nuvarande inloggad användare. och skickar in den till listan vi skapa ovan.
             var friends = FriendsRepository.GetFriends(IdentityHelper.CurrentUserId());
             foreach (var friend in friends)
             {
                 var newFriend = new FriendsModel(friend.Id, friend.Username);
                 friendList.Add(newFriend);
             }
-            //skickar med listan in i vyn
+
             return View(friendList);
         }
 
-        //skapar en vy för FriendRequest
+        
         public ActionResult FriendRequest()
         {
-
-            //hämtar alla vänförfrågningar för nuvarande inloggad användare och för in dem i en lista av typen FriendRequestModel
+            
             var friendRequests = FriendsRepository.GetFriendRequest(IdentityHelper.CurrentUserId());
             List<FriendRequestModel> friendRequestModels = new List<FriendRequestModel>();
             foreach (var f in friendRequests)
@@ -87,14 +77,13 @@ namespace DatingSite_Projekt.Controllers
                 friendRequestModels.Add(friend);
                 
             }
-            //skickar med listan in i vyn
+            
             return View(friendRequestModels);
         }
 
-       //skapar en funktion för att logga ut
+      
         public ActionResult LogOut()
         {
-            //tar bort kakan och skicka användaren till Login vyn i HomeController
             FormsAuthentication.SignOut();
             return RedirectToAction("Login", "Home");
         }
@@ -103,43 +92,43 @@ namespace DatingSite_Projekt.Controllers
         [HttpPost]
         public ActionResult UploadImage(HttpPostedFileBase file)
         {
-            //kollar så filens värde inte är null
+
             if (file != null)
             {
-                //slår ihop serverns väg där filen skapas och döper om filen till användarnamnet.jpg
-                string path = Path.Combine(
-                              Server.MapPath("~/Images/Profile"), UserRepositories.GetUserName(IdentityHelper.CurrentUserId()) + ".jpg");
-                //skapar en array som innehåller två strängar, strängarna används för att verifiera att endast.jpg bilder är tillåtna
-                var allowedExtensions = new[] { ".jpg", ".JPG" };
+
                 var extension = Path.GetExtension(file.FileName);
+                //slår ihop serverns väg där filen skapas och döper om filen till användarnamnet + tidigare extension
+                string path = Path.Combine(
+                              Server.MapPath("~/Images/Profile"), UserRepositories.GetUserName(IdentityHelper.CurrentUserId()) + extension);
+
+
+                var allowedExtensions = new[] { ".jpg", ".JPG", ".jpeg" };
                 if (!allowedExtensions.Contains(extension))
                 {
-                    //skapar ett felmeddelande som skickas till Edit kontrollern
+                    
                     TempData["failed/succeded"] = "Upload failed! Only .jpg filetypes allowed!";
                     RedirectToAction("Edit", "Account", ModelState);
                 }
                 else
                 { 
-                    //om allt gått bra så sparas filen lokalt med variabeln path som sökväg
+                //om allt gått bra så sparas filen lokalt med variabeln path som sökväg
+                TempData["failed/succeded"] = "Upload was successful";
                 file.SaveAs(path);
                 }
             }
-            // efter uppladdning så skickas du till Edit kontrollern med ett meddelande
-            TempData["failed/succeded"] = "Upload was successful";
+
+
             return RedirectToAction("Edit", "Account");
         
         }
 
-       //skapar Edit vyn
+
         public ActionResult Edit()
         {
-            //skapar en modell för typen AccountUserModel
+            
             var model = new AccountUserModel();
-            //Hämtar information från nuvarande inloggad användare
             var results = DescriptionRepository.GetDescription(IdentityHelper.CurrentUserId());
-            //konverterar en nullable int från resultatet till en vanlig int så den kan användas i modellen
             int age = results.Age ?? default(int);
-            //fyller modellen med den hämtade informationen
             model.Description = results.Description1;
             model.AboutMe = results.AboutMe;
             model.Age = age; 
@@ -152,21 +141,20 @@ namespace DatingSite_Projekt.Controllers
             {
                 ViewData["UploadMessage"] = TempData["failed/succeded"] as string;
             }
-            //returnerar vyn med en medskickad modell
+            
             return View(model);
         }
 
-        //hanterar när information från vyn sparas, använder en AccountUserModel som parameter att fylla information i
+
         [HttpPost]
         public ActionResult Edit(AccountUserModel model)
         {
-            //kollar så att alla fält är ifyllda korrekt enligt modellens annotations, skickas tillbaka till vyn med felmeddelande annars
+            
             if (!ModelState.IsValid)
             {
                 return View();
             }
-            //ifall allt går igenom skickas den sparade informationen till datalagret för att föras in i tabellerna och du returneras
-            //sedan tillbaka till vyn med ett meddelande att förändringarna har lyckats
+            
             DescriptionRepository.InsertNewDescription(model.Description, model.AboutMe, model.Country, model.City, model.Email, model.Age, IdentityHelper.CurrentUserId());
             UserRepositories.SetUserSearchable(model.Searchable, IdentityHelper.CurrentUserId());
             ViewData["Success"] = "Changes to your profile has been made!";
@@ -174,13 +162,13 @@ namespace DatingSite_Projekt.Controllers
 
         }
 
-        //skapar en vy för EditUserInfo som används för att ändra lösenord och användarnamn
+
         public ActionResult EditUserInfo()
         {
             return View();
         }
 
-        //använder en AccountUserInfoModel för att fylla i korrekt information i vyn och skickas sedan till denna metod som parameter
+       
         [HttpPost]
         public ActionResult EditUserInfo(AccountUserInfoModel model)
         {
@@ -188,20 +176,22 @@ namespace DatingSite_Projekt.Controllers
             var userId = IdentityHelper.CurrentUserId();
             var currentUserPassword = UserRepositories.GetPasswordForUser(userId);
             var comparison = UserRepositories.GetUsers();
-            //kollar så alla annotations stämmer och returnerar i annat fall en vy med felmeddelanden
+            
             if (!ModelState.IsValid)
             {
                 return View();
             }
+
             //Kontrollerar så det gamla lösenordet överrensstämmer med det som finns lagrat i databasen för användaren.
             if (!model.OldPassword.Equals(currentUserPassword))
             {
                 ModelState.AddModelError("", "Wrong password!");
                 return View(model);
             }
+
             foreach (var user in comparison)
             {
-                //kollar så användarnamnet inte redan existerar och jämför inte med användarens nuvarande användarnamn.
+
                 if (user.Username.Equals(model.NewUsername) && !model.NewUsername.Equals(UserRepositories.GetUserName(userId)))
                 {
                     ModelState.AddModelError("", "Username already exists!");
@@ -209,34 +199,35 @@ namespace DatingSite_Projekt.Controllers
                 }
 
             }
+
             //Ifall inget nytt användarnamn är ifyllt fylls ditt gamla in.
             if (model.NewUsername == null)
             {
                 model.NewUsername = UserRepositories.GetUserName(userId);
             }
+
             //Ifall du inte har infört några nya detaljer till ditt användarnamn eller lösenord så kommer ett meddelande fram.
-            if (model.NewUsername.Equals(UserRepositories.GetUserName(userId)) && model.NewPassword == null)
+            else if (model.NewUsername.Equals(UserRepositories.GetUserName(userId)) && model.NewPassword == null)
             {
                 ModelState.AddModelError("", "You've made no changes to your account!");
                 return View(model);
             }
+
             //Ifall du endast byter användarnamn och inte tillför något nytt lösenord byts ditt användarnamn.
-            if (!model.NewUsername.Equals(UserRepositories.GetUserName(userId)) && model.NewPassword == null)
+            else if (!model.NewUsername.Equals(UserRepositories.GetUserName(userId)) && model.NewPassword == null)
             {
                 ModelState.AddModelError("", "Username is updated!");
                 UserRepositories.SetUserChanges(model.NewUsername, userId);
                 return View(model);
             }
 
-            //ifall allt har gått vägen så skickas informationen till datalagret för att sparas och användaren
-            //skickas tillbaka med vyn med ett meddelande om att allt gått bra
+          
             ModelState.AddModelError("", "Username and Password or just password has been updated!");
             UserRepositories.SetUserChanges(model.NewPassword, model.NewUsername, userId);
             return View(model);
         }
 
-        //skapar Details vyn, avsedd för andra användares/vänners profiler
-        //tar emot id't på användaren vars vy du vill se
+
         public ActionResult Details(int id)
         {
 
@@ -245,14 +236,11 @@ namespace DatingSite_Projekt.Controllers
             {
                 return RedirectToAction("Profile", "Account");
             }
-            //omvandlar id't till en nullable int för att datalagret ska kunna matcha det mot tabellen
-            //skapar en modell av AccountUserModel vid namn usersPage och hämtar informationen från descriptions tabellen för användaren
-            //vars vy du ska se
+
             int? userId = id;
             var usersPage = new AccountUserModel();
             var results = DescriptionRepository.GetDescription(userId);
-            //kollar så att resultatet inte är null och kör isådannafall nedanstående kod som fyller modellen med resultatets information
-                //konverterar en nullable int från resultatet till en vanlig int så den kan användas i modellen
+
                 int age = results.Age ?? default(int);
                 usersPage.Description = results.Description1;
                 usersPage.AboutMe = results.AboutMe;
@@ -271,6 +259,7 @@ namespace DatingSite_Projekt.Controllers
                     var message = new MessageModel { Message = m.Messages, SenderName = UserRepositories.GetUserName(m.SenderId), SenderId = m.SenderId };
                     messageList.Add(message);
                 }
+
                 //skickar med listan, nuvarande användares id och id't vars vy du ska se i ViewData som hämtas i vyn
                 //används för att lista meddelanden och sedan för att skicka dem. Du returneras sedan till vyn Details med modellen för 
                 //användaren
@@ -282,23 +271,23 @@ namespace DatingSite_Projekt.Controllers
            
         }
 
-        // returnerar vyn för SearchUsers
+
         public ActionResult SearchUsers()
         {
             return View();
         }
 
-        //hanterar informationen som skickats med från vyn ovan
+      
         [HttpPost]
         public ActionResult SearchUsers(SearchModel model)
         {
-            //kollar så modellen är korrekt skickar annars tillbaka till vyn
+
             if (!ModelState.IsValid)
             {
                 return View();
 
             }
-            //skapar en lista vid namn resultat och hämtar sedan alla användare i databasen
+
             var result = new List<SearchModel>();
             var users = UserRepositories.GetUsers();
 
@@ -308,8 +297,7 @@ namespace DatingSite_Projekt.Controllers
                 model.Username = "";
             }
 
-            //loopar igenom användarna som hämtades från databasen och kontrollerar så att användarnamnet startar med sökvärdet och att 
-            //användarna har valt att vara sökbara, de som matchar värdet läggs till i result listan
+
             foreach (var u in users)
             {
 
@@ -326,7 +314,7 @@ namespace DatingSite_Projekt.Controllers
             return RedirectToAction("SearchUserResult", "Account");
         }
        
-        //skapar vyn för resultatet av sökningen
+
         public ActionResult SearchUserResult()
         {
             //hämtar ut TempDatan som skickades med från ovanståend ActionResult och skickar med listan till vyn
